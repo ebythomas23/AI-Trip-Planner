11589ff9020acdde9935341ca925668a326b7f3d
Eby <ebykachappillil@gmail.com>
2025-08-21 22:26:53 +1000

    drafted streamlit ui initial structure

---
+++ app_streamlit.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import datetime
+import requests
+import sys
+
+#Backend endpoint
+BASE_URL = "http://localhost:8000"  
+
+st.set_page_config()
+
+
+st.title()
+
+
+st.header()
+
+
+
+with st.form():
+    submit_button =
+    user_input =
+
+
+
+if submit_button and user_input.strip():
+
