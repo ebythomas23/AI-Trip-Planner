26571b404139de75423884fd7a14433e56d3d9d6
Eby <ebykachappillil@gmail.com>
2025-08-23 18:34:04 +1000

    implemented tool logic for  currency conversion

---
+++ tools/currency_conversion_tool.py
@@ -0,0 +1,23 @@
+from utils.currency_convertor import CurrencyConvertor
+import os 
+from typing import List
+from langchain.tools import tool
+from dotenv import load_dotenv
+
+class CurrencyConvertorTool:
+    def __init__(self) -> None:
+        load_dotenv()
+        self.api_key=os.environ.get("EXCHANGE_RATE_API_KEY")
+        self.currency_service = CurrencyConvertor(api_key=self.api_key)
+        self.currency_converter_tool_list = self._setup_tools()
+
+    def _setup_tools(self)->List:
+        """ Setup tools for currency convertor tool"""
+
+        @tool
+        def convert_currency(amount:float, from_currency:str, to_currency:str):
+            """ Conert amount from one currency to another"""
+            return self.currency_service(amount, from_currency, to_currency)
+        
+
+        return [convert_currency]
